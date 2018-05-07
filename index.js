503de0f49f60eca34f6812805a488095b6887b82
pwwpche <pwwpche@gmail.com>
2018-05-07 16:56:34 -0700

    Add prefix to multi entrance module resource name

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ function WebpackNameModuleId(options) {
     this.options = options || {};
 }
 
-WebpackNameModuleId.prototype.apply = function(compiler) {
+WebpackNameModuleId.prototype.constructor = function(options) {
+    this.prefix = options.prefix ? '' : options.prefix;
+};
 
+WebpackNameModuleId.prototype.apply = function(compiler) {
     compiler.plugin("compilation", function(compilation) {
         compilation.plugin("after-optimize-module-ids", function(modules) {
             modules.forEach(function(module) {
@@ -13,16 +16,18 @@ WebpackNameModuleId.prototype.apply = function(compiler) {
                 if (resourceName) {
                     if (resourceName.indexOf('node_modules/') !== -1) {
                         module.id = resourceName.substr(resourceName.lastIndexOf('node_modules/') + 'node_modules/'.length);
+                        return;
                     } else if (resourceName.indexOf('src/') !== -1) {
                         module.id = resourceName.substr(resourceName.lastIndexOf('src/') + 'src/'.length);
+                    } else if (resourceName.indexOf('app/') !== -1) {
+                        module.id = resourceName.substr(resourceName.lastIndexOf('app/') + 'app/'.length);
                     }
+                    module.id = this.prefix + module.id;
                 }
             });
         });
     });
 };
-
-
 
 module.exports = WebpackNameModuleId;
 

8b61940f14d7bdc8d28accb94ba67a55149148c8
pwwpche <pwwpche@gmail.com>
2018-08-14 09:03:36 -0700

    Improve error message

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 var crypto = require('crypto');
 var fs = require('fs');
 
-function checksum (str) {
-    return crypto
-        .createHash('md5')
-        .update(str, 'utf8')
-        .digest('hex')
+function checksum(str) {
+    return crypto.createHash('md5').update(str, 'utf8').digest('hex')
 }
 
 
@@ -13,11 +10,21 @@ function WebpackNameModuleId(options) {
     this._options = options;
 }
 
-var packageDependencies = require('../../package-lock.json').dependencies;
+var packageDependencies = {};
+try {
+    packageDependencies = require('../../package-lock.json').dependencies;
+} catch (e) {
+    console.error(
+        '[WebpackNameModuleIdPlugin] Cannot find package-lock.json, skip adding package version to module id. \n' +
+        'Notice: Module version is added in module id by default. Missing version number may cause your build' +
+        'incompatible with other builds.'
+    );
+}
+
 function getVersionOfPackage(resourcePath) {
     var pathArr = resourcePath.split('/');
     var version = '', currentPackageName = '';
-    for( var packageName of pathArr) {
+    for (var packageName of pathArr) {
         currentPackageName += (currentPackageName ? '/' : '') + packageName;
         if (packageDependencies.hasOwnProperty(currentPackageName)) {
             version = packageDependencies[currentPackageName].version;
@@ -31,18 +38,24 @@ function replaceModuleId(module, modulePrefix) {
     var replacedId = module.id;
 
     if (resourceName && resourceName.indexOf('node_modules/') !== -1) {
-        replacedId = resourceName.substr(resourceName.lastIndexOf('node_modules/') + 'node_modules/'.length);
+        replacedId = resourceName.substr(
+            resourceName.lastIndexOf('node_modules/') + 'node_modules/'.length);
         var checkSumStr = '';
-        try{
+        try {
             const file = fs.readFileSync(resourceName);
             checkSumStr = checksum(file);
-        } catch (e) {}
+        } catch (e) {
+            console.error('Checksum failed for file:' + filename);
+            console.error(e);
+        }
         replacedId += '_' + getVersionOfPackage(replacedId) + '_' + checkSumStr;
     } else if (resourceName) {
-        if ( resourceName.indexOf('src/') !== -1) {
-            replacedId = resourceName.substr(resourceName.lastIndexOf('src/') + 'src/'.length);
+        if (resourceName.indexOf('src/') !== -1) {
+            replacedId =
+                resourceName.substr(resourceName.lastIndexOf('src/') + 'src/'.length);
         } else if (resourceName.indexOf('app/') !== -1) {
-            replacedId = resourceName.substr(resourceName.lastIndexOf('app/') + 'app/'.length);
+            replacedId =
+                resourceName.substr(resourceName.lastIndexOf('app/') + 'app/'.length);
         }
     }
 
@@ -53,21 +66,20 @@ function replaceModuleId(module, modulePrefix) {
     return replacedId;
 }
 
-WebpackNameModuleId.prototype.apply = function(compiler) {
+WebpackNameModuleId.prototype.apply = function (compiler) {
     var modulePrefix = this._options.prefix || '';
     var skipPrefixForVendors = this._options['skip-prefix-for-vendors'] || true;
-    compiler.plugin("compilation", function(compilation) {
-        compilation.plugin("after-optimize-chunk-ids", function(chunks) {
-
-            chunks.forEach(function(chunk) {
-                const prefix = (skipPrefixForVendors && chunk.name === 'vendor') ? '' : modulePrefix;
-                console.log(chunk.name, prefix);
+    compiler.plugin('compilation', function (compilation) {
+        compilation.plugin('after-optimize-chunk-ids', function (chunks) {
+            chunks.forEach(function (chunk) {
+                const prefix = (skipPrefixForVendors && chunk.name === 'vendor') ?
+                    '' :
+                    modulePrefix;
                 chunk.forEachModule(function (module) {
                     module.id = replaceModuleId(module, prefix);
                 });
             });
         });
-
     });
 };
 
